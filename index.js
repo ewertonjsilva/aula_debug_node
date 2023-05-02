655ee9d055b079503cc69b4a98d04521778b56d7
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-05-02 14:41:00 -0300

    aula 02-05

--- index.js
+++ index.js
@@ -11,19 +11,27 @@ let idade = parseInt(idadeTxt);
 
 let resposta = 'n';
 
-if (idade < 15) {
-    resposta = entrada('Acredito que você esteja no ensino fundamental. \nEstou certo? (responda s/n)');
-} else if (idade > 15 && idade < 18) {
-    resposta = entrada('Acredito que você esteja no ensino médio. \nEstou certo? (responda s/n)');
-} else {
-    resposta = entrada('Acredito que você já tenha terminado o ensino médio. \nEstou certo? (responda s/n)');
-}
+do {
 
-if (resposta === 'n') {
-    console.log('Ok eu não sei de tudo!');
-} else {
-    console.log('Legal, quando eu crescer vou ser igual ao CHAT GPT!');
-}
+    if (idade < 15) {
+        resposta = entrada('Acredito que você esteja no ensino fundamental. \nEstou certo? (responda s/n)');
+    } else if (idade > 15 && idade < 18) {
+        resposta = entrada('Acredito que você esteja no ensino médio. \nEstou certo? (responda s/n)');
+    } else {
+        resposta = entrada('Acredito que você já tenha terminado o ensino médio. \nEstou certo? (responda s/n)');
+    }
+
+    resposta = resposta.toLocaleLowerCase();
+
+    if (resposta === 'n') {
+        console.log('Ok eu não sei de tudo!');
+    } 
+
+    if (resposta === 's') {
+        console.log('Legal, quando eu crescer vou ser igual ao CHAT GPT!');
+    }
+    
+} while (resposta !== 'n' && resposta !== 's')
 
 console.log('');
 entrada('Fim do programa, pressione ENTER para encerrar!');
